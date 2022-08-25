104fd0f566b213c90a695a121f5f26a8be3c1471
WispySparks <wispysparks@gmail.com>
2022-08-25 01:36:02 -0500

    j

--- src/main/java/com/wispy/ArrowTracer/enchantments/ArrowTracerEnchantment.java
+++ src/main/java/com/wispy/ArrowTracer/enchantments/ArrowTracerEnchantment.java
@@ -1,6 +1,11 @@
 package com.wispy.ArrowTracer.enchantments;
 
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
@@ -10,4 +15,9 @@ public class ArrowTracerEnchantment extends Enchantment {
         super(rarity, enchantmentCategory, equipmentSlots);
     }
     
+    @Override
+    public void doPostHurt(LivingEntity attacker, Entity target, int level) {
+        EntityType.LIGHTNING_BOLT.spawn((ServerLevel) attacker.level, null, null, target.blockPosition(), MobSpawnType.TRIGGERED, true, true);
+    }
+
 }
