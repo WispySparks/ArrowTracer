231ca40031d7fe413ce2198f224f9159e5a9b7f1
WispySparks <wispysparks@gmail.com>
2022-08-24 22:53:05 -0500

    enchantmnet

--- src/main/java/com/wispy/ArrowTracer/ArrowTracer.java
+++ src/main/java/com/wispy/ArrowTracer/ArrowTracer.java
@@ -3,20 +3,31 @@ package com.wispy.ArrowTracer;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
+import com.wispy.ArrowTracer.enchantments.ArrowTracerEnchantment;
 import com.wispy.ArrowTracer.events.Events;
 
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod(ArrowTracer.modID)
 public class ArrowTracer {
 
     public static final String modID = "arrowtracer";
     public static final Logger logger = LogUtils.getLogger();
+    public static final DeferredRegister<Enchantment> enchantments = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, modID);
+    public static RegistryObject<Enchantment> tracingEnchantment = enchantments.register("tracing", 
+    () -> new ArrowTracerEnchantment(Enchantment.Rarity.RARE, EnchantmentCategory.BOW, EquipmentSlot.MAINHAND));
 
     public ArrowTracer() {
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(new Events());
+        enchantments.register(MinecraftForge.EVENT_BUS);
     }
 
 }
---
+++ src/main/java/com/wispy/ArrowTracer/enchantments/ArrowTracerEnchantment.java
@@ -0,0 +1,13 @@
+package com.wispy.ArrowTracer.enchantments;
+
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+
+public class ArrowTracerEnchantment extends Enchantment {
+
+    public ArrowTracerEnchantment(Rarity rarity, EnchantmentCategory enchantmentCategory, EquipmentSlot... equipmentSlots) {
+        super(rarity, enchantmentCategory, equipmentSlots);
+    }
+    
+}
