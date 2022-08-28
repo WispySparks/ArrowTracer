00ad2e3cb8c8150ed1c0749753157ecfdd3aea4f
WispySparks <wispysparks@gmail.com>
2022-08-28 14:08:47 -0500

    only craftable and findable

--- src/main/java/com/wispy/ArrowTracer/enchantment/TracingEnchantment.java
+++ src/main/java/com/wispy/ArrowTracer/enchantment/TracingEnchantment.java
@@ -1,6 +1,7 @@
 package com.wispy.ArrowTracer.enchantment;
 
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
@@ -10,6 +11,26 @@ public class TracingEnchantment extends Enchantment {
 
     public TracingEnchantment(EquipmentSlot... pApplicableSlots) {
         super(Rarity.RARE, EnchantmentCategory.BOW, pApplicableSlots);
+    }
+
+    @Override
+    public boolean canEnchant(ItemStack pStack) {
+        return pStack.canApplyAtEnchantingTable(this);
+    }
+
+    @Override
+    public boolean canApplyAtEnchantingTable(ItemStack stack) {
+        return false;
+    }
+
+    @Override
+    public boolean isTradeable() {
+        return false;
+    }
+
+    @Override
+    public boolean isAllowedOnBooks() {
+        return false;
     }
     
 }
