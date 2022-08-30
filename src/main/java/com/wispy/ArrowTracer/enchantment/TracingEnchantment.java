3f1d07db0da15928fae37711e31d460be22f8b1e
WispySparks <wispysparks@gmail.com>
2022-08-29 20:06:00 -0500

    book is in tab now

--- src/main/java/com/wispy/ArrowTracer/enchantment/TracingEnchantment.java
+++ src/main/java/com/wispy/ArrowTracer/enchantment/TracingEnchantment.java
@@ -1,10 +1,14 @@
 package com.wispy.ArrowTracer.enchantment;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
+@SuppressWarnings("resource")
 public class TracingEnchantment extends Enchantment {
 
     public static final Enchantment tracingEnchantment =  new TracingEnchantment(EquipmentSlot.MAINHAND);
@@ -28,9 +32,25 @@ public class TracingEnchantment extends Enchantment {
         return false;
     }
 
-    @Override
+    @Override 
     public boolean isAllowedOnBooks() {
+        if (Minecraft.getInstance().player.isCreative()) {
+            return true;
+        }
         return false;
+    }
+
+    @Override
+    public boolean allowedInCreativeTab(Item book, CreativeModeTab tab) {
+        Enchantment eBook = (Enchantment) this;
+        if (tab == CreativeModeTab.TAB_SEARCH)
+        {
+            return eBook.category != null;
+        }
+        else
+        {
+            return tab.hasEnchantmentCategory(eBook.category);
+        }
     }
     
 }
